8cdd75f61bb94a2813f74190456ccf49967f3602
Luke de Jong <ldejong9938@gmail.com>
2023-01-11 16:16:14 +0100

    Comments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+//Imports (Import opdrachten uit de des betreffende opdrachten)
+
 import { RunOpdracht1 } from "./Opdracht_1.js"
-import { RunOpdracht10 } from "./Opdracht_10.js"
 import { RunOpdracht2 } from "./Opdracht_2.js"
 import { RunOpdracht3 } from "./Opdracht_3.js"
 import { RunOpdracht4 } from "./Opdracht_4.js"
@@ -8,8 +9,9 @@ import { RunOpdracht6 } from "./Opdracht_6.js"
 import { RunOpdracht7 } from "./Opdracht_7.js"
 import { RunOpdracht8 } from "./Opdracht_8.js"
 import { RunOpdracht9 } from "./Opdracht_9.js"
+import { RunOpdracht10 } from "./Opdracht_10.js"
 
-const opdrachten: Record<string, () => void> = {
+const opdrachten: Record<string, () => void> = { //Object die per record een string en een void terug geeft
     "opdracht1": RunOpdracht1,
     "opdracht2": RunOpdracht2,
     "opdracht3": RunOpdracht3,
@@ -21,6 +23,6 @@ const opdrachten: Record<string, () => void> = {
     "opdracht9": RunOpdracht9,
     "opdracht10": RunOpdracht10
 }
-const opdracht = process.argv[2]
+const opdracht = process.argv[2] //Pak argument 2 (Het derde woord van de command)
 
-opdrachten[opdracht]?.()
+opdrachten[opdracht]?.() //Kijk of het 2e argument voor komt in "opdrachten" en voer deze uit, zo niet (?) doe niks
