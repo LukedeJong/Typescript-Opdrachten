0b2434c9b3ee868a946364471538423d33102bb7
Luke de Jong <ldejong9938@gmail.com>
2023-01-11 16:58:51 +0100

    Eerste opdracht gemaakt (Plus)

--- src/Opdracht_1.ts
+++ src/Opdracht_1.ts
@@ -1,3 +1,14 @@
 export function RunOpdracht1() {
-    console.log("Hallo Wereld");
+    
+    let antwoord = 0;
+
+    function cijfers(cijfer1: number, cijfer2: number) {
+
+        antwoord = cijfer1 + cijfer2;
+
+        console.log(cijfer1, "+" ,cijfer2, "=", antwoord)
+        return antwoord;
+    }
+    
+    cijfers(15,11);
 }
